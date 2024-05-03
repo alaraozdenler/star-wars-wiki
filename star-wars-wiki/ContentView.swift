b24806778bc3f6ed05d567baf816b1ec2886c2f5
Alara Özdenler <alaraozdenler@Alaras-Air.fritz.box>
2024-05-03 15:15:43 +0200

    initial commit

---
+++ star-wars-wiki/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  star-wars-wiki
+//
+//  Created by Alara Özdenler on 03.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ star-wars-wiki/star_wars_wikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  star_wars_wikiApp.swift
+//  star-wars-wiki
+//
+//  Created by Alara Özdenler on 03.05.24.
+//
+
+import SwiftUI
+
+@main
+struct star_wars_wikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
